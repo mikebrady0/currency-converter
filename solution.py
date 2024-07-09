9a6fb23ed8313d879ccda1f8e0caa225f66b295d
mikebrady0 <147462552+mikebrady0@users.noreply.github.com>
2024-07-09 19:22:25 -0400

    this is the first commit
    Currency converter commit

--- solution.py
+++ solution.py
@@ -1,1 +1,16 @@
-# This is the main text for the currency calculator
+import requests
+
+from_currency = str(
+    input("Enter in the currency you'd like to convert: ")).upper()
+
+to_currency = str(
+    input("Enter in thecurrency you'd like to convert to: ")).upper()
+
+amount = float(input("Enter in the amount of money: "))
+
+response = requests.get(
+    f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}")
+
+
+print(
+    f"{amount} {from_currency} is {response.json()['rates'][to_currency]} {to_currency}")
